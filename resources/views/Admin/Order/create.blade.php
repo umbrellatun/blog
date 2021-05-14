af67d6b5a24f7418616b9f70b8dd42536df147b5
jtit <innovation.jtpack@gmail.com>
2021-05-14 17:24:17 +0700

    Update create.blade.php

--- resources/views/Admin/Order/create.blade.php
+++ resources/views/Admin/Order/create.blade.php
@@ -397,6 +397,18 @@
                                   column4 += data.price_lak;
                                   column5 += data.product_type.name;
                                   column6 += data.in_stock;
+                                  column7 += '<div class="btn-group w-75" role="group" aria-label="Basic example">';
+                                  column7 += '<button type="button" class="btn btn-danger btn-number btn-sm" data-type="minus" data-field="quant['+key+']" title="หยิบออกจากรถเข็น">';
+                                  column7 += '<span class="fas fa-minus-circle"></span>';
+                                  column7 += '</button>';
+                                  column7 += '<button type="button" class="btn btn-success btn-number btn-sm" data-type="plus" data-field="quant['+key+']" title="หยิบใส่รถเข็น">';
+                                  column7 += '<span class="fas fa-cart-plus"></span>';
+                                  column7 += '</button>';
+                                  column7 += '</div>';
+                                  column7 += '<div class="form-group">';
+                                  column7 += '<input type="text" name="quant['+key+']" id="product_id_'+data.id+'" class="w-75 input-number number-only form-control" value="0" min="0" max="'+data.in_stock+'" data-value="'+data.id+'">';
+                                  column7 += '</div>';
+
                                   $("#simpletable").DataTable().row.add([column1, column2, column3, column4, column5, column6, column7]).draw();
 
                                   img = '';
@@ -407,6 +419,120 @@
                                   column5 = '';
                                   column6 = '';
                                   column7 = '';
+                             });
+
+                             $('.btn-number').click(function(e){
+                                  e.preventDefault();
+                                  fieldName = $(this).attr('data-field');
+                                  type      = $(this).attr('data-type');
+                                  var input = $("input[name='"+fieldName+"']");
+                                  var currentVal = parseInt(input.val());
+                                  if (!isNaN(currentVal)) {
+                                       if(type == 'minus') {
+
+                                            if(currentVal > input.attr('min')) {
+                                                 input.val(currentVal - 1).change();
+                                            }
+                                            if(parseInt(input.val()) == input.attr('min')) {
+                                                 $(this).attr('disabled', true);
+                                            }
+
+                                       } else if(type == 'plus') {
+
+                                            if(currentVal < input.attr('max')) {
+                                                 input.val(currentVal + 1).change();
+                                            }
+                                            if(parseInt(input.val()) == input.attr('max')) {
+                                                 $(this).attr('disabled', true);
+                                            }
+
+                                       }
+                                  } else {
+                                       input.val(0);
+                                  }
+                             });
+                             $('.input-number').focusin(function(){
+                                  $(this).data('oldValue', $(this).val());
+                             });
+                             $('.input-number').change(function() {
+                                  minValue =  parseInt($(this).attr('min'));
+                                  maxValue =  parseInt($(this).attr('max'));
+                                  valueCurrent = parseInt($(this).val());
+                                  product_id = $(this).data("value");
+                                  $.ajax({
+                                       method : "post",
+                                       url : '{{ route('order.get_product') }}',
+                                       dataType : 'json',
+                                       data: {"product_id" : product_id, "valueCurrent" : valueCurrent},
+                                       beforeSend: function() {
+                                            $("#preloaders").css("display", "block");
+                                       },
+                                  }).done(function(rec){
+                                       $("#preloaders").css("display", "none");
+                                       let tr = '';
+                                       if(rec.status==1){
+                                            if ($('#table_cart').find("#row_"+rec.product_id).length == 1){
+                                                 $("#row_" + product_id).remove();
+                                            }
+                                            if (valueCurrent == 0) {
+                                                 $("#row_" + product_id).remove();
+                                            } else {
+                                                 tr += '<tr id="row_'+rec.product_id+'">';
+                                                 tr += '<td>';
+                                                 tr += '<div class="d-inline-block align-middle">';
+                                                 tr += '<img src="'+url_gb+'/uploads/products/'+rec.image+'" alt="user image" class="img-radius align-top m-r-15" style="width:40px;">';
+                                                 tr += '<input type="hidden" name="product_id[]" value="'+rec.product_id+'">';
+                                                 tr += '<input type="hidden" name="product_amount[]" value="'+valueCurrent+'">';
+                                                 tr += '</div>';
+                                                 tr += '</td>';
+                                                 tr += '<td>'+rec.sku+'</td>';
+                                                 tr += '<td>'+rec.name+'</td>';
+                                                 tr += '<td class="text-right">'+addNumformat((rec.price_bath).toFixed(2))+'</td>';
+                                                 tr += '<td class="text-right">'+addNumformat((rec.price_lak).toFixed(2))+'</td>';
+                                                 tr += '<td><span id="product_amount_'+rec.product_id+'">'+valueCurrent+'<span></td>';
+                                                 tr += '<td class="text-right">'+addNumformat((rec.sum_bath).toFixed(2))+'</td>';
+                                                 tr += '<td class="text-right">'+addNumformat((rec.sum_lak).toFixed(2))+'</td>';
+                                                 tr += '</tr>';
+                                                 $("#table_cart > tbody:last").append(tr);
+                                            }
+                                       } else {
+                                            swal("", rec.content, "warning");
+                                            $("#product_id_"+ product_id).val(rec.amount);
+                                       }
+                                  }).fail(function(){
+                                       $("#preloaders").css("display", "none");
+                                       swal("", rec.content, "error");
+                                  });
+
+                                  name = $(this).attr('name');
+                                  if(valueCurrent >= minValue) {
+                                       $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+                                  } else {
+                                       // alert('Sorry, the minimum value was reached');
+                                       $(this).val($(this).data('oldValue'));
+                                  }
+                                  if(valueCurrent <= maxValue) {
+                                       $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+                                  } else {
+                                       // alert('Sorry, the maximum value was reached');
+                                       $(this).val($(this).data('oldValue'));
+                                  }
+
+                             });
+                             $(".input-number").keydown(function (e) {
+                                  // Allow: backspace, delete, tab, escape, enter and .
+                                  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+                                  // Allow: Ctrl+A
+                                  (e.keyCode == 65 && e.ctrlKey === true) ||
+                                  // Allow: home, end, left, right
+                                  (e.keyCode >= 35 && e.keyCode <= 39)) {
+                                       // let it happen, don't do anything
+                                       return;
+                                  }
+                                  // Ensure that it is a number and stop the keypress
+                                  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                                       e.preventDefault();
+                                  }
                              });
                         }
 
@@ -474,7 +600,6 @@
               $('.input-number').focusin(function(){
                    $(this).data('oldValue', $(this).val());
               });
-
               $('.input-number').change(function() {
                    minValue =  parseInt($(this).attr('min'));
                    maxValue =  parseInt($(this).attr('max'));
